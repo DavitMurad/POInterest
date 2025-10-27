0b0ee2881fa0a361efe6ce5079e37d0e4636a101
“Davit <2840850M@student.gla.ac.uk>
2025-10-27 16:45:31 +0000

    Initial commit

---
+++ POInterest/POInterest/POInterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  POInterestApp.swift
+//  POInterest
+//
+//  Created by Davit Muradyan on 27.10.25.
+//
+
+import SwiftUI
+
+@main
+struct POInterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
